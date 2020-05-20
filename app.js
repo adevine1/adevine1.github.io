8d203ad06327a7ef2b9abd7d459e352e6e5afcd9
Aaron Devine <a.f.devine@gmail.com>
2020-05-19 17:11:38 -0700

    minor changes to main home page

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 function initialize () {
-      let x = Math.floor ((Math.random () * 3) + 1);
-      if (x == 2) {
+      let x = Math.floor ((Math.random () * 4) + 1);
+      if (x == 4) {
         document.getElementById("container1").style.backgroundColor='lightsteelblue';
       } else if (x==3) {
         document.getElementById("container1").style.backgroundColor='lightyellow';
-      } else
+      } else if (x=2) {
         document.getElementById("container1").style.backgroundColor="lightgreen";
+      } else
+        document.getElementById("container1").style.backgroundColor="lightskyblue";
 }
