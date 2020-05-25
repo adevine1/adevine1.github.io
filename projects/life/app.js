534b13c84400db4577b8765c921c1f894a020c41
Aaron Devine <a.f.devine@gmail.com>
2020-05-24 22:35:14 -0700

    working on interface, js code, and css styling for game

--- projects/life/app.js
+++ projects/life/app.js
@@ -4,11 +4,7 @@
 // 2. Any dead cell with three live neighbors becomes a live cell.
 // 3. All other live cells die in the next generation. Similarly, all other dead cells stay dead.
 
-function initialize () {
-  for (let x=0; x<15; x++)
-    for (let y=0; y<15; y++)
-      array [x][y] = "*";
-}
+
 
 
 var array = [
@@ -29,5 +25,11 @@ var array = [
   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
 ]
 
-initialize ();
-console.log (array);
+function initialize () {
+  for (let x=0; x<15; x++)
+    for (let y=0; y<15; y++)
+      array [x][y] = "*";
+  console.log (array);
+}
+
+
