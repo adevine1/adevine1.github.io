e060164d2d33988e431e17476bae1b3351539aec
Aaron Devine <a.f.devine@gmail.com>
2020-12-07 15:45:04 -0800

    added descriptive comments to js code

--- projects/bookmarks/app.js
+++ projects/bookmarks/app.js
@@ -3,8 +3,10 @@ $(document).ready(function () {
   alert("ready!");
   console.log('document ready');
 
+  //starts the document with the assumption that the New Boomark creation box is closed
   var newbookmarkgrouphidden = false;
 
+  //toggles the New Bookmark creation box
   $("#addbookmark").click(function () {
     if (newbookmarkgrouphidden === false) {
       $("#newbookmarkgroup").show();
@@ -16,12 +18,13 @@ $(document).ready(function () {
     }
   });
 
+  //closes the New Bookmark creation box
   $("#closebutton").click(function () {
     $("#newbookmarkgroup").hide();
     newbookmarkgrouphidden = false;
   })
 
-  //need to frame function this way, as adding elements via jQuery does not add element to the dom
+  //need to frame function this way, as adding elements via jQuery does not add element to the dom.
   //otherwise, jQuery does not recognize button clicks for added elements
   $(document).on('click', '.removegroup', (function () {
     let self = this.id;
@@ -35,11 +38,14 @@ $(document).ready(function () {
     }
   }))
 
+  //this is the ID random number generator. Big enough that repeats not likely.
   function getRandomNumber(x) {
-    let rand = Math.floor(Math.random() * 100000) + 1000;
+    let rand = Math.floor(Math.random() * 100000000) + 1000;
     return rand;
   }
 
+  //this is the function that creates a default New Bookmark Group
+  //it gets a random number for its id, and takes the groupname and description from the New Bookmark box
   $("#creategroup").click(function () {
     let rand = getRandomNumber();
     var groupname = $('#bookmarkgroupname').val();
