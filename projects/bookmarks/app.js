17bd45de453ae37b4c5eb10261609c1d0e66cd24
Aaron Devine <a.f.devine@gmail.com>
2020-12-07 15:04:39 -0800

    created code to add and remove new bookmark groups

--- projects/bookmarks/app.js
+++ projects/bookmarks/app.js
@@ -7,23 +7,23 @@ $(document).ready(function () {
 
   $("#addbookmark").click(function () {
     if (newbookmarkgrouphidden === false) {
-      $("#newbookmarkgroup").show ();
+      $("#newbookmarkgroup").show();
       console.log("hit addbookmark")
       newbookmarkgrouphidden = true;
     } else {
-      $("#newbookmarkgroup").hide ();
+      $("#newbookmarkgroup").hide();
       newbookmarkgrouphidden = false;
     }
   });
 
   $("#closebutton").click(function () {
-    $("#newbookmarkgroup").hide ();
+    $("#newbookmarkgroup").hide();
     newbookmarkgrouphidden = false;
   })
 
-  $(".removegroup").click(function () {
-    let self=this.id;
-    console.log ('removegroup button clicked');
+  $(document).on('click', '.removegroup', (function () {
+    let self = this.id;
+    console.log('removegroup button clicked');
     console.log(self);
     let check = confirm(
       "Are you sure that you want to delete this bookmark group?"
@@ -31,19 +31,27 @@ $(document).ready(function () {
     if (check == true) {
       $(`#top${self}`).remove();
     }
-  })
+  }))
+
+  function getRandomNumber(x) {
+    let rand = Math.floor(Math.random() * 100000) + 1000;
+    return rand;
+  }
 
   $("#creategroup").click(function () {
-    $("#maincontainer").append(`<div class="card" id="top2">
+    let rand = getRandomNumber();
+    var groupname = $('#bookmarkgroupname').val();
+    $("#cardcontainer").prepend (`<div class="card" id="top${rand}">
     <div class="card-body">
-      <h5 class="card-title">Daily</h5>
+      <h5 class="card-title">${groupname}</h5>
       <p class="card-text"><a href="https://news.google.com/topstories">Google News</a></p>
       <p class="card-text"><a href="http://www.google.com">Google</a></p>
       <p class="card-text"><a href="http://www.freecodecamp.org">Free Code Camp</a></p>
       <a href="#" class="btn btn-primary">Add Bookmark</a>
-      <a href="#" class="btn btn-primary removegroup" id="2">Remove Group</a>
+      <a href="#" class="btn btn-primary removegroup" id="${rand}">Remove Group</a>
     </div>
   </div>`)
   })
+
 
 });
