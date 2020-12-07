10fcd329c520df121065377439b15d9c000d1349
Aaron Devine <a.f.devine@gmail.com>
2020-12-07 12:09:35 -0800

    added functionality to bookmark page that adds new bookmark group on button press; proof of concept at this point

--- projects/bookmarks/app.js
+++ projects/bookmarks/app.js
@@ -25,8 +25,25 @@ $(document).ready(function () {
     let self=this.id;
     console.log ('removegroup button clicked');
     console.log(self);
-    $(`#top${self}`).remove();
+    let check = confirm(
+      "Are you sure that you want to delete this bookmark group?"
+    );
+    if (check == true) {
+      $(`#top${self}`).remove();
+    }
   })
 
+  $("#creategroup").click(function () {
+    $("#maincontainer").append(`<div class="card" id="top2">
+    <div class="card-body">
+      <h5 class="card-title">Daily</h5>
+      <p class="card-text"><a href="https://news.google.com/topstories">Google News</a></p>
+      <p class="card-text"><a href="http://www.google.com">Google</a></p>
+      <p class="card-text"><a href="http://www.freecodecamp.org">Free Code Camp</a></p>
+      <a href="#" class="btn btn-primary">Add Bookmark</a>
+      <a href="#" class="btn btn-primary removegroup" id="2">Remove Group</a>
+    </div>
+  </div>`)
+  })
 
 });
