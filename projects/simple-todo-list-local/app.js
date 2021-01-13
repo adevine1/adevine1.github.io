7369c5ba979f8e44e15b4e7ba139b8ee382cf54c
Aaron Devine <a.f.devine@gmail.com>
2021-01-13 12:09:28 -0800

    preliminary work to set up new to-do list program

---
+++ projects/simple-todo-list-local/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+  console.log ("ready!");
+
+  $(document).on('click', '#add-item-button', (function () {
+    console.log ('clicked add-item-button');
+  }));
+
+});
