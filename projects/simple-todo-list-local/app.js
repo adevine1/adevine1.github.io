243155fc80aa809912977a35ae92359f00591219
Aaron Devine <a.f.devine@gmail.com>
2021-01-13 15:03:21 -0800

    added delete functionality to items added to todo-list-local

--- projects/simple-todo-list-local/app.js
+++ projects/simple-todo-list-local/app.js
@@ -2,22 +2,42 @@ $(document).ready(function () {
 
   console.log ("ready!");
 
+  function getRandomNumber(x) {
+    let rand = Math.floor(Math.random() * 100000000) + 1000000;
+    return rand;
+  }
+
   $(document).on('click', '#add-item-button', (function () {
     console.log ('clicked add-item-button');
+    let texty = $('#create-entry').val();
+    let itemid = getRandomNumber();
+    console.log(itemid);
+    console.log(texty);
 
     $('#item-list').append(`
-
-    <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-    <span class="item-list">text!</span>
+    <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between" id="${itemid}">
+    <span class="item-list">${texty}</span>
     <div>
-      <button id="12345" class="edit-me btn btn-outline-secondary btn-sm mr-1">edit</button>
-      <button id="12345" class="delete-me btn btn-outline-danger btn-sm">remove</button>
+      <button id="edit-${itemid}" class="edit-me btn btn-outline-secondary btn-sm mr-1">edit</button>
+      <button id="delete-${itemid}" class="delete-me btn btn-outline-danger btn-sm">remove</button>
     </div>
   </li>`);
   }));
 
   $(document).on('click', '#print-todo', (function () {
     window.print ();
+  }));
+
+  $(document).on('click', '.delete-me', (function () {
+      console.log('addlink button clicked');
+      let self = this.id;
+      console.log(self);
+      let dashlocation = self.indexOf('-');
+      console.log('the dash is at location ' + dashlocation + '.');
+      let baseid = self.substring(dashlocation + 1, self.length);
+      console.log (baseid);
+      $(`#${baseid}`).remove();
+      console.log('id removed');
   }));
 
 });
